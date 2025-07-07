ec21d59f569d00fa07fb71a86acac3af282b4481
Mark <brandonnarciso24@gmail.com>
2025-07-06 22:00:00 -0700

    feat: Complete the loading logic using useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import "./App.css";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchedGif, setFetchedGif] = useState(false);
+  const kakyoin = useRef("");
+  const giphyURL = "https://api.giphy.com/v1/gifs/";
+  const kakyoinId = "wUf0jPd2ksY92";
+  const giphyAPIkey = "BR2DuOOkBFz9u2pzDHyKSRgQHfk8gzSQ";
+
+  //! This is for development only
+  const didFetch = useRef(false);
+
+  useEffect(() => {
+    if (didFetch.current) return;
+    didFetch.current = true;
+    async function fetchGif() {
+      try {
+        const response = await fetch(
+          `${giphyURL}${kakyoinId}?api_key=${giphyAPIkey}`
+        );
+
+        const contentType = response.headers.get("content-type");
+        if (!contentType || !contentType.includes("application/json")) {
+          throw new TypeError("Oops, we haven't got JSON!");
+        }
+        const json = await response.json();
+        kakyoin.current = json.data.images.original.url;
+        setFetchedGif(true);
+      } catch (error) {
+        console.log("Error: ", error);
+        setIsLoading(false);
+      }
+    }
+    fetchGif();
+  }, []);
+
+  useEffect(() => {
+    if (fetchedGif) {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [fetchedGif, isLoading]);
+
+  return <>{isLoading ? <div>True</div> : <div>False</div>}</>;
 }
 
 export default App;
