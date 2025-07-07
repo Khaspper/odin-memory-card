76c32bef59c7615ac761b5f119e0929c84938d36
Mark <brandonnarciso24@gmail.com>
2025-07-06 22:50:18 -0700

    feat: finish implementing the loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [fetchedGif, setFetchedGif] = useState(false);
-  const kakyoin = useRef("");
-  const giphyURL = "https://api.giphy.com/v1/gifs/";
-  const kakyoinId = "wUf0jPd2ksY92";
-  const giphyAPIkey = "BR2DuOOkBFz9u2pzDHyKSRgQHfk8gzSQ";
-
-  //! This is for development only
-  const didFetch = useRef(false);
 
   useEffect(() => {
-    if (didFetch.current) return;
-    didFetch.current = true;
-    async function fetchGif() {
-      try {
-        const response = await fetch(
-          `${giphyURL}${kakyoinId}?api_key=${giphyAPIkey}`
-        );
-
-        const contentType = response.headers.get("content-type");
-        if (!contentType || !contentType.includes("application/json")) {
-          throw new TypeError("Oops, we haven't got JSON!");
-        }
-        const json = await response.json();
-        kakyoin.current = json.data.images.original.url;
-        setFetchedGif(true);
-      } catch (error) {
-        console.log("Error: ", error);
-        setIsLoading(false);
-      }
-    }
-    fetchGif();
-  }, []);
-
-  useEffect(() => {
-    if (fetchedGif) {
+    if (isLoading) {
       const timer = setTimeout(() => {
         setIsLoading(false);
       }, 3000);
       return () => clearTimeout(timer);
     }
-  }, [fetchedGif, isLoading]);
+  }, [isLoading]);
 
-  return <>{isLoading ? <div>True</div> : <div>False</div>}</>;
+  return <>{isLoading ? <LoadingScreen /> : <div>False</div>}</>;
 }
 
 export default App;
---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,12 @@
+export default function LoadingScreen() {
+  return (
+    <div
+      id="loading-screen"
+      className="relative w-[100vw] h-[100vh] flex justify-start items-center"
+    >
+      <h1 className="text-4xl py-3 px-4 rounded-2xl bg-emerald-600 text-black">
+        Loading...
+      </h1>
+    </div>
+  );
+}
